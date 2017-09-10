24cbb4fcd2e52c4fad843f6a2f78f5bf59d1e7f4
hy yang <almond2yang@gmail.com>
2017-09-10 15:45:20 -0700

    a

--- INFO5100/gittest.java
+++ INFO5100/gittest.java
@@ -3,7 +3,7 @@ public class gittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("come on");
+		System.out.println("come on github");
 
 	}
 
