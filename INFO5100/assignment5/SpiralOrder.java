fe33a9acf7d9d9fbd1ea383722510711d614d99e
hy yang <almond2yang@gmail.com>
2017-10-10 16:27:35 -0700

    add extra

--- INFO5100/assignment5/SpiralOrder.java
+++ INFO5100/assignment5/SpiralOrder.java
@@ -5,8 +5,36 @@ import java.util.*;
 public class SpiralOrder {
     
     public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> x=new ArrayList<>();
-        return x;
-    }
+        ArrayList<Integer> so=new ArrayList<>();
+        if (matrix.length == 0) return so;
+        int m=matrix.length-1;
+        int n=matrix[0].length-1;
+        int x=0;
+        int y=0;
+        while (x<=n-x && y<=m-y) {
+            if (so.size()==(m+1)*(n+1)) return so;
+            if (x==n-x && y<m-y) {
+                for (int i=y;i<=m-y;i++) so.add(matrix[i][x]);
+                break;
+            }
+            if (y==m-y && x<n-x) {
+                for (int i=x;i<=n-x;i++) so.add(matrix[y][i]);
+                break;
+            }
+            if (y==m-y && x==n-x) {
+                so.add(matrix[x][y]);
+                break;
+            }
+            if (x>n-x && y>m-y) break;
+            
+            for (int i=x;i<n-x;i++) so.add(matrix[y][i]);
+            for (int i=y;i<m-y;i++) so.add(matrix[i][n-x]);
+            for (int i=n-x;i>x;i--) so.add(matrix[m-y][i]);
+            for (int i=m-y;i>y;i--) so.add(matrix[i][x]);
+            x++;
+            y++;            
+        }
+        return so;
+    }   
     
 }
