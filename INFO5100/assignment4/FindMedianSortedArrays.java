aac0c6ae893384a0808df53a78f0b39040f90328
hy yang <almond2yang@gmail.com>
2017-10-18 14:12:06 -0700

    "stupid" brute way: combine 2 arrays, then find median

--- INFO5100/assignment4/FindMedianSortedArrays.java
+++ INFO5100/assignment4/FindMedianSortedArrays.java
@@ -6,20 +6,37 @@ public class FindMedianSortedArrays {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        int[] a= {1};
+        int[] b= {1};
+        System.out.println(findMedianSortedArrays(a,b));
+        
 
     }
     
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1.length==0) return FindMedian(nums2);
-        if (nums2.length==0) return FindMedian(nums1);
+        if (nums2.length==0) return FindMedian(nums1);        
         
-        int[] l=nums1;
-        int[] s=nums2;        
-        if (nums1.length<nums2.length) {
-            l=nums2;
-            s=nums1;
+        // "stupid" brute way: combine 2 arrays, then find median
+        int[] aux=new int[nums1.length+nums2.length];
+        int i=0,j=0,a=0;
+        while (i<nums1.length && j<nums2.length) {
+            if (nums1[i]<nums2[j]) {
+                aux[a++]=nums1[i++];
+            }
+            else aux[a++]=nums2[j++];
         }
-        
+        if (i<nums1.length) {
+            while (i<nums1.length) {
+                aux[a++]=nums1[i++];
+            }
+        }
+        if (j<nums2.length) {
+            while (j<nums2.length) {
+                aux[a++]=nums2[j++];
+            }
+        }
+        return  FindMedian(aux);
     }
     
     static double FindMedian(int[] arr) {
@@ -29,11 +46,4 @@ public class FindMedianSortedArrays {
         return Double.NaN;
     }
     
-    static int FindMedianIndex(int[] arr) {        
-        if (arr.length==0) return -1;        
-        if (arr.length%2==0) return arr.length/2;
-        if (arr.length%2==1) return (arr.length-1)/2;
-        return -1;
-    }
-
 }
