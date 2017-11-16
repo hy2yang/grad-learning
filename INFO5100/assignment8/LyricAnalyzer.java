046515392083a535ac0f5d29cf6465dcdb56c726
hy yang <almond2yang@gmail.com>
2017-11-15 23:17:21 -0800

    q2

--- INFO5100/assignment8/LyricAnalyzer.java
+++ INFO5100/assignment8/LyricAnalyzer.java
@@ -2,13 +2,13 @@ package assignment8;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class LyricAnalyzer {
     
@@ -21,17 +21,21 @@ public class LyricAnalyzer {
     public void read(File file) throws IOException{
         FileReader fr=new FileReader(file);
         BufferedReader br=new BufferedReader(fr);
-        int pos=0;
-        while (true){
-            String[] line=br.readLine().split(" ");
-            for (int i=0;i<line.length;++i){
-                if(i==line.length-1){
-                    add(line[i],-pos);
+        int pos=1;
+        String line;
+        while ((line=br.readLine())!=null){
+            //if (br.readLine()==null) break;
+            String[] words=line.toUpperCase().split(" ");            
+            for (int i=0;i<words.length;++i){
+                if(i==words.length-1){
+                    add(words[i],-pos);
                 }
-                else add(line[i],pos);
+                else add(words[i],pos);
                 ++pos;
             }  
         }
+        br.close();
+        fr.close();
     }
     
     private void add(String lyricWord, int wordPosition){        
@@ -39,7 +43,9 @@ public class LyricAnalyzer {
             map.get(lyricWord).add(wordPosition);
         }
         else {
-            map.put(lyricWord, new ArrayList<Integer>(wordPosition));
+            ArrayList<Integer> pos=new ArrayList<>();
+            pos.add(wordPosition);
+            map.put(lyricWord, pos);
         }
     }
     
@@ -51,14 +57,35 @@ public class LyricAnalyzer {
             StringBuilder sb=new StringBuilder();
             sb.append(s);
             sb.append(": ");
-            sb.append(map.get(s));
+            String list=map.get(s).toString();
+            sb.append(list.substring(1, list.length()-1));
             System.out.println(sb.toString());
         }
         System.out.println();
     }
     
-    public void writeLyrics(File file){
-        
+    public void writeLyrics(File file) throws IOException{
+        int num=0;
+        for (ArrayList<Integer> a:map.values()) {
+            num+=a.size();
+        }
+        String[] words=new String[num+1];
+        Arrays.fill(words,"");
+        for (String s:map.keySet()) {
+            for (Integer i: map.get(s)) {
+                if (i<0) {
+                    words[-i]=s.toUpperCase()+" "+System.lineSeparator();
+                }
+                else words[i]=s.toUpperCase()+" ";
+            }
+        }
+        FileWriter writer = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(writer);
+        for (int i=1;i<words.length;++i) {
+            pw.print(words[i]);
+        }
+        pw.close();
+        writer.close();
     }
     
     public int count(){
@@ -78,6 +105,22 @@ public class LyricAnalyzer {
     }
 
     public static void main(String[] args) {
+        LyricAnalyzer la=new LyricAnalyzer();
+        try {
+            la.read(new File("q2t3.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(la.count()+" unique words in the lyric");
+        System.out.println(la.mostFrequentWord()+" is the most frequent");
+        System.out.println();
+        la.displayWords();
+        File nfile=new File("newlyrics.txt");
+        try {
+            la.writeLyrics(nfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
     }
     
